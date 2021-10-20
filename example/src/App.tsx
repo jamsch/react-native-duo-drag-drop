edcb949c3f81a2eb6015a8c6564dce696c2a83ec
jamsch <jamsch18@student.wintec.ac.nz>
2021-10-20 14:59:54 +1300

    chore(example): bump react-native to 0.66.1

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,7 +8,7 @@ import DuoDragDrop, {
   Lines,
   DuoAnimatedStyleWorklet,
 } from "@jamsch/react-native-duo-drag-drop";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
 import { withSpring, withTiming } from "react-native-reanimated";
 
 const customAnimatedStyle: DuoAnimatedStyleWorklet = (style, isGestureActive) => {
@@ -32,79 +32,84 @@ const customAnimatedStyle: DuoAnimatedStyleWorklet = (style, isGestureActive) =>
 export default function App() {
   const [rtl, setRtl] = useState(false);
   const [gradeWords, setGradeWords] = useState<boolean[]>([]);
-  const [gesturedDisabled, setGesturesDisabled] = useState(false);
-  const [answeredWords, setAnsweredWords] = useState<string[]>([]);
+  const [gesturesDisabled, setGesturesDisabled] = useState(false);
+  const [answeredWords, setAnsweredWords] = useState<string[] | null>(null);
   const [shouldUseCustomWorket, setShouldUseCustomWorket] = useState(false);
   const duoDragDropRef = useRef<DuoDragDropRef>(null);
 
   return (
     <GestureHandlerRootView style={styles.container}>
-      <View style={styles.dragDropContainer}>
-        <DuoDragDrop
-          ref={duoDragDropRef}
-          words={["Juan", "She", "apples", "today", "with", "eats", "her", "another"]}
-          wordHeight={45}
-          wordGap={4}
-          gesturesDisabled={gesturedDisabled}
-          rtl={rtl}
-          wordBankOffsetY={20}
-          wordBankAlignment="center"
-          extraData={gradeWords}
-          animatedStyleWorklet={shouldUseCustomWorket ? customAnimatedStyle : undefined}
-          renderWord={(_word, index) => (
-            <Word
-              containerStyle={
-                typeof gradeWords?.[index] === "boolean" && {
-                  backgroundColor: gradeWords?.[index] ? "green" : "red",
-                  borderColor: gradeWords?.[index] ? "green" : "red",
+      <ScrollView style={styles.container}>
+        <View style={styles.dragDropContainer}>
+          <DuoDragDrop
+            ref={duoDragDropRef}
+            words={["Juan", "She", "apples", "today", "with", "eats", "her", "another"]}
+            wordHeight={40}
+            lineHeight={49}
+            wordGap={4}
+            gesturesDisabled={gesturesDisabled}
+            rtl={rtl}
+            wordBankOffsetY={10}
+            wordBankAlignment="center"
+            extraData={gradeWords}
+            animatedStyleWorklet={shouldUseCustomWorket ? customAnimatedStyle : undefined}
+            renderWord={(_word, index) => (
+              <Word
+                containerStyle={
+                  typeof gradeWords?.[index] === "boolean" && {
+                    backgroundColor: gradeWords?.[index] ? "green" : "red",
+                    borderColor: gradeWords?.[index] ? "green" : "red",
+                  }
                 }
-              }
-              textStyle={{
-                color: typeof gradeWords?.[index] === "boolean" ? "white" : "black",
+                textStyle={{
+                  color: typeof gradeWords?.[index] === "boolean" ? "white" : "black",
+                }}
+              />
+            )}
+            renderPlaceholder={({ style }) => <Placeholder style={[style, { borderRadius: 5 }]} />}
+            renderLines={(props) => (
+              <Lines
+                {...props}
+                containerStyle={{ backgroundColor: "transparent" }}
+                lineStyle={{ borderColor: "#CCC" }}
+              />
+            )}
+          />
+          <Button
+            title="Get answered words"
+            onPress={() => setAnsweredWords(duoDragDropRef.current?.getAnsweredWords() || [])}
+          />
+          {answeredWords && (
+            <View style={{ marginTop: 10 }}>
+              <Text>{JSON.stringify(answeredWords)}</Text>
+            </View>
+          )}
+          <View style={{ marginTop: 10 }}>
+            <Button
+              title="Grade words"
+              onPress={() => {
+                if (gradeWords.length > 0) {
+                  setGradeWords([]);
+                } else {
+                  setGradeWords([true, false, true, false, false, true, false, false]);
+                }
               }}
             />
-          )}
-          renderPlaceholder={({ style }) => <Placeholder style={[style, { borderRadius: 5 }]} />}
-          renderLines={(props) => (
-            <Lines {...props} containerStyle={{ backgroundColor: "transparent" }} lineStyle={{ borderColor: "#CCC" }} />
-          )}
-        />
-      </View>
-      <View style={{ margin: 10 }}>
-        <Button
-          title="Get answered words"
-          onPress={() => setAnsweredWords(duoDragDropRef.current?.getAnsweredWords() || [])}
-        />
-        {answeredWords.length > 0 && (
-          <View style={{ marginTop: 10 }}>
-            <Text>{answeredWords.join(", ")}</Text>
           </View>
-        )}
-        <View style={{ marginTop: 10 }}>
-          <Button
-            title="Grade words"
-            onPress={() => {
-              if (gradeWords.length > 0) {
-                setGradeWords([]);
-              } else {
-                setGradeWords([true, false, true, false, false, true, false, false]);
-              }
-            }}
-          />
+          <View style={{ marginTop: 10 }}>
+            <Button title={`Gestures disabled: ${gesturesDisabled}`} onPress={() => setGesturesDisabled((s) => !s)} />
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Button
+              title={`Use custom animations: ${shouldUseCustomWorket}`}
+              onPress={() => setShouldUseCustomWorket((s) => !s)}
+            />
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Button title={`Right-To-Left: ${rtl}`} onPress={() => setRtl((s) => !s)} />
+          </View>
         </View>
-        <View style={{ marginTop: 10 }}>
-          <Button title={`Gestures disabled: ${gesturedDisabled}`} onPress={() => setGesturesDisabled((s) => !s)} />
-        </View>
-        <View style={{ marginTop: 10 }}>
-          <Button
-            title={`Use custom animations: ${shouldUseCustomWorket}`}
-            onPress={() => setShouldUseCustomWorket((s) => !s)}
-          />
-        </View>
-        <View style={{ marginTop: 10 }}>
-          <Button title={`Right-To-Left: ${rtl}`} onPress={() => setRtl((s) => !s)} />
-        </View>
-      </View>
+      </ScrollView>
     </GestureHandlerRootView>
   );
 }
@@ -115,6 +120,6 @@ const styles = StyleSheet.create({
   },
   dragDropContainer: {
     margin: 20,
-    minHeight: 300,
+    flex: 1,
   },
 });
